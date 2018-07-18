458dae0055cd518f897e6c7dfa03d46130f6d1df
RaghulDaniel.T <raghuldaniel.t@cognizant.com>
2018-07-18 13:07:18 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -2,72 +2,147 @@ function sendMessage(event, socket) {
 	event.preventDefault();
 	let chatContainer = document.getElementById('chatContainer');
 	let message = document.getElementById('message').value;
-	chatContainer.querySelectorAll('div.sent')[0].getElementsByTagName('p')[0].innerHTML = 'Me : ' + message;
-	socket.emit('message',message);
-	$('#sentMessage').show();
+	let username = document.getElementById('username').value;
+	let channel = document.getElementById('channel').value;
+	var div = document.createElement('div');
+	div.classList.add('col-12', 'message');
+	div.innerHTML = `	
+				<div class="card alert-warning">
+					<div class="card-body">
+						<p class="card-text">Me : ${message}</p>
+					</div>
+				</div>
+			 `;
+
+	let channelMessage = {};
+	channelMessage.message = message;
+	channelMessage.channel = channel;
+	channelMessage.username = username;
+	socket.emit('message', channelMessage);
+	chatContainer.insertBefore(div, chatContainer.childNodes[0]);
 }
 
 function joinChannel(event, socket) {
 	event.preventDefault();
 	let channelsInpt = document.getElementById('newchannel').value;
-	let channel = channelsInpt === '' ? [] : channelsInpt.split(',');
-	console.log("add channel",channel);
-	socket.emit('joinChannel', channel);
+	console.log("add channel", channelsInpt);
+	let channelMessage = {};
+	channelMessage.channel = channelsInpt;
+	socket.emit('joinChannel', channelMessage);
 
 }
 
 function leaveChannel(event, socket) {
 	event.preventDefault();
 	let channelsInpt = document.getElementById('newchannel').value;
-	let channel = channelsInpt === '' ? [] : channelsInpt.split(',');
-
-	// let channeList = document.getElementById('channelsList');
-	// console.log(channeList.selectedIndex);
-	// channeList.remove(channeList.selectedIndex);
-	// document.getElementById('newchannel').value = '';
-	// console.log('channeList', channeList);
-	socket.emit('leaveChannel', channel);
+	console.log("add channel", channelsInpt);
+	let channelMessage = {};
+	channelMessage.channel = channelsInpt;
+	socket.emit('leaveChannel', channelMessage);
 }
 
 function onWelcomeMessageReceived(message) {
-	console.log('message', message);
+	console.log('mess', message);
 	let chatContainer = document.getElementById('chatContainer');
-	chatContainer.querySelectorAll('div.system')[0].getElementsByTagName('p')[0].innerHTML = 'System : Welcome ' + message.username + '!!';
-	$('#systemMessage').show();
+	var div = document.createElement('div');
+	div.classList.add('col-12','message');
+	div.innerHTML = `
+				<div class="card alert-primary">
+					<div class="card-body">
+						<p class="card-text">System : ${message}</p>
+					</div>
+				</div>
+			`;
+	chatContainer.insertBefore(div, chatContainer.childNodes[0]);
 
 }
 
-function onNewMessageReceived() {
+function onNewMessageReceived(message) {
+	let chatContainer = document.getElementById('chatContainer');
+	var div = document.createElement('div');
+	div.classList.add('col-12','message');
+	div.innerHTML = `
+				<div class="card alert-success">
+					<div class="card-body">
+						<p class="card-text">${message.username} : ${message.message}</p>
+					</div>
+				</div>
+			`
+	chatContainer.insertBefore(div, chatContainer.childNodes[0]);
 }
 
 function onAddedToNewChannelReceived(message) {
-	buildChannelsHTML(message, 'You are added to <strong>' + message + '</strong> successfully!');
+	let divMessage =`You are added to <strong>${message.channel}</strong> successfully!`;
+	buildChannelsHTML(message, true,divMessage);
 }
 
 function onRemovedFromChannelReceived(message) {
-
-	buildChannelsHTML(message, 'You are removed from <strong>' + message + '</strong> successfully!');
+	let divMessage =`You are removed from <strong>${message.channel}</strong> successfully!`;
+	buildChannelsHTML(message, false,divMessage);
 
 
 }
 
-function buildChannelsHTML(message, textMessage) {
-	$('#channelAlert').show();
-	document.getElementById('channelAlert').innerText = textMessage;
-	if (message != '' && message.length > 0) {
-		console.log('aaa', message);
+function buildChannelsHTML(message, isAdd,divMessage) {
+	console.log('mess', message);
+	var div = document.createElement('div');
+	let alertStyle = 'alert-danger';
+	if(isAdd){
+		alertStyle = 'alert-success';
+	}
+	var att = document.createAttribute("role");
+	att.value = "alert";
+	div.setAttributeNode(att);
+	div.classList.add('alert', 'alert-success', 'alert-dismissible', 'fade', 'show',alertStyle);
+	div.innerHTML = `
+	<div >`+divMessage+`		
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			</button>
+	</div>`;
+	let alertCont = document.getElementById('alertContainer');
+	alertCont.appendChild(div);
+	if (message.channel != '') {
+
 		let channeList = document.getElementById('channelsList');
-		let options;
-		message.map(function (channel) {
-			options += `
-			<option>${channel}</option>
-			` ;
-		});
-		console.log(options);
-		channeList.innerHTML = options;
+		let options = '';
+
+		if (Array.isArray(message.channel)) {
+			channeList.innerHTML = '';
+			message.channel.map(function (value) {
+				options += `
+								<option>${value}</option>
+							` ;
+			});
+			channeList.innerHTML = options;
+		} else {
+
+			if (isAdd) {
+				var option = document.createElement('option');
+				option.value = message.channel;
+				channeList.appendChild(option);
+			} else {
+				remSelOpt(message.channel, channeList);
+			}
+		}
+
+
+		document.getElementById('newchannel').value = '';
 	}
 
 }
+
+
+function remSelOpt(val, channeList) {
+	let len = channeList.options.length;
+	for (i = 0; i < len; i++) {
+		if (channeList.options[i].value == val) {
+			channeList.children[i].remove();
+			break;
+		}
+	}
+}
+
 
 module.exports = {
 	sendMessage,
